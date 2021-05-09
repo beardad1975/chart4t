1edb3149031e7f5a8d0edcc1d09ae3d1d8b33705
beardad1975 <beardad1975@nmes.tyc.edu.tw>
2021-05-09 17:43:07 +0800

    first commit

---
+++ chart4t/__init__.py
@@ -0,0 +1,18 @@
+
+
+from . import common
+
+__all__ = [ 
+            ''
+            ]
+
+# init
+#common.speaker = win32com.client.Dispatch("SAPI.SpVoice")
+
+
+
+
+
+if __name__ == '__main__' :
+    pass
+    
